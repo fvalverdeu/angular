e3a70512b7721a3b26f9f900b1aec2ad54a6d1c1
Fernando Valverde Uchoffen <fvalverdeu@gmail.com>
2019-08-10 23:59:14 -0500

    Final changes

--- PruebasAngular8/src/app/app.component.ts
+++ PruebasAngular8/src/app/app.component.ts
@@ -1,26 +1,46 @@
 import { Component } from '@angular/core';
 
+import { PostService } from './posts.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [PostService]
 })
+
 export class AppComponent {
   name: string;
   email: string;
   website: string;
   hobbies: string[];//los tipos son opcionales
   showHobbies: boolean;
+  posts: IPost[]
 
-  constructor(){
+  constructor(private postService:PostService){
     this.name = 'Fernando';
     this.email = "fvalverdeu@gmail.com";
     this.website = "http://www.fatzweb.com";
     this.hobbies = ["Lectura", "fútbol", "cine"];
     this.showHobbies = false;
+    this.postService.getPosts().subscribe(posts => {
+      this.posts = posts as any;
+    });
   }
 
   toggleHobbies(){
     this.showHobbies = !this.showHobbies;
   }
+
+  newHobby(hobby) {
+    this.hobbies.push(hobby.value);
+    hobby.value = '';
+    return false;
+  }
+}
+
+interface IPost {
+  id: string;
+  title: string;
+  body: string;
 }
